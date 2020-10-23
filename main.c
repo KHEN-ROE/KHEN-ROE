1d5b8813fc827b6d5b74a132d440a2bf1ddeb716
repl.it user <replituser@example.com>
2020-10-23 04:54:56 +0000

    nohh

--- main.c
+++ main.c
@@ -2,7 +2,9 @@
 int main()
 {
 
-printf("두 번째 10월 23일\n);
-printf("두 번째 두 번째 10월 23일");
-printf("세 번째 10월 23일\n")
+printf("두 번째 10월 23일\n");
+printf("두 번째 두 번째 10월 23일\n");
+printf("세 번째 10월 23일\n");
+
+return 0;
 }
